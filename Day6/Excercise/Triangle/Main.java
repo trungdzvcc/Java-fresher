493b18b23f71519155ddcabd71a1602e767e0f5b
trungdzvcc <maitrungmnt01@gmail.com>
2022-07-27 09:05:35 +0700

    recapture the test cases

--- Day6/Excercise/Triangle/Main.java
+++ Day6/Excercise/Triangle/Main.java
@@ -1,13 +1,37 @@
 package Day6.Excercise.Triangle;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        Triangle t = new Triangle(5,6,7);
-        System.out.println(t);
-        System.out.println("Area of triangle: " + t.getArea());
-        System.out.println("Perimeter of triangle: " + t.getPerimeter());
 
-    }
-   
+  public static void main(String[] args) {
+    double side1;
+    double side2;
+    double side3;
+    double t2 = 0;
 
+    do {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter the side 1 : ");
+      side1 = scanner.nextDouble();
+      System.out.print("Enter the side 2 : ");
+      side2 = scanner.nextDouble();
+      System.out.print("Enter the side 3 : ");
+      side3 = scanner.nextDouble();
+      if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+        System.out.println("\nDont't enter < 0 \n");
+      } else {
+        Triangle t = new Triangle(side1, side2, side3);
+        t2 = t.getPerimeter();
+
+        if (t2 == 6) {
+          System.out.println("\nDont't enter three values : 1 , 2 ,3 \n");
+        } else {
+          System.out.println(t);
+          System.out.println("Perimeter of triangle: " + t2);
+          System.out.println("Area of triangle: " + t.getArea());
+        }
+      }
+    } while (side1 <= 0 || side2 <= 0 || side3 <= 0 || t2 == 6);
+  }
 }
--- Day6/Excercise/Triangle/Triangle.java
+++ Day6/Excercise/Triangle/Triangle.java
@@ -30,37 +30,54 @@ public class Triangle extends Shape{
         this.side3 = side3;
     }
     public double getArea() {
+        // half perimeter
         double ah = getPerimeter()/2;
-        return  Math.sqrt(ah* (ah - side1) * (ah - side2) * (ah - side3)); 
-        // chua bat xong tat ca tam giac
-        // double ch , c1 ,c2;
-        // if(side1>side2&&side1>side3){
-        //     ch = side1;
-        //     c1=side2;
-        //     c2=side3;
-        // }else if(side2>side3&&side2>side1){
-        //     ch=side2;
-        //     c1=side1;
-        //     c2=side3;
-        // }else{
-        //     ch = side3;
-        //     c1=side1;
-        //     c2=side2;
-        // }
 
-        // if(side1 == side2 || side1 == side3 || side2 ==side3) {
-        //     double h = Math.sqrt(Math.pow(ch,2)-);
-            
-        // }else if(side1 == side2 && side1 == side3){
-        //     return side1*(Math.sqrt(3)/2);
-        // }else if(){
-        //     double halfP = (side1+side2+side3) / 2;
-        //         double Area = Math.sqrt(halfP  * (halfP - side1) * (halfP - side2) * (halfP - side3));
-        //     return Area;
-        // }else{
+        // find hypotenuse
+        double ch , c1 ,c2;
+        if(side1>side2&&side1>side3){
+            ch = side1;
+            c1=side2;
+            c2=side3;
+        }else if(side2>side3&&side2>side1){
+            ch=side2;
+            c1=side1;
+            c2=side3;
+        }else{
+            ch = side3;
+            c1=side1;
+            c2=side2;
+        }
+        //isosceles triangle
+        //condition that the two parties are congruent
+        if(side1 == side2 || side1 == side3 || side2 ==side3) {
+            double bottom = side1;
+            double evenSide = side2 ;
+            if(bottom>side2){
+                bottom = side2;
+                evenSide = side1;        
+            }else{
+                bottom = side3;
+                evenSide = side1;  
+            }
+            // caculate height
+            double height = Math.sqrt(Math.pow(evenSide,2)- Math.pow(bottom,2));
+            //caculate area
+            return (bottom*height)/2;
            
-        //     return  2 * (Math.sqrt(p*(p-side1)*(p-side2)*(p-side3))/side1);
-        // }
+        }else if(side1 == side2 && side1 == side3){  
+            //equilateral triangle
+            //condition that the three parties are congruent
+            //caculate area
+            return side1*(Math.sqrt(3)/2);
+        }else if(Math.pow(ch,2)== Math.pow(c1,2)+Math.pow(c2,2)){
+            //square triangle
+            // condition pytago theorem
+            return (c1*c2)/2;
+        }else{
+           // triangle
+            return Math.sqrt(ah*(ah-side1)*(ah-side2)*(ah-side3))/side1;
+        }
     
     }
     
