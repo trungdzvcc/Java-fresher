84f8ce97193c3e03840a84be9b9b2cb76d1b4266
trungdzvcc <maitrungmnt01@gmail.com>
2022-07-27 09:20:58 +0700

    test case check side of triagle

--- Day6/Excercise/Triangle/Main.java
+++ Day6/Excercise/Triangle/Main.java
@@ -24,14 +24,14 @@ public class Main {
         Triangle t = new Triangle(side1, side2, side3);
         t2 = t.getPerimeter();
 
-        if (t2 == 6) {
-          System.out.println("\nDont't enter three values : 1 , 2 ,3 \n");
+        if (side1 +side2  <= side3) {
+          System.out.println("\nValues just enter is not the side of triangle  \n");
         } else {
           System.out.println(t);
           System.out.println("Perimeter of triangle: " + t2);
           System.out.println("Area of triangle: " + t.getArea());
         }
       }
-    } while (side1 <= 0 || side2 <= 0 || side3 <= 0 || t2 == 6);
+    } while (side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 +side2  <= side3);
   }
 }
