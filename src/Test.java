ecea229d464332452737f5455cf03f6145077639
trungdzvcc <maitrungmnr01@gmail.com>
2022-08-22 08:17:12 +0700

    Day16

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Test {
+public class Test {
+    public static void main(String[] args){
+        String s="abc";
+        int i=Integer.parseInt(s);//NumberFormatException
+
+    }
 }
